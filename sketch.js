82ef4bc14e10bec52ba5e390e7e6c82c5cb34864
AdamSpannbauer <spannbaueradam@gmail.com>
2020-10-01 18:29:31 -0400

    add player class

--- sketch.js
+++ sketch.js
@@ -2,17 +2,45 @@ const canvasW = 512;
 const canvasH = 512;
 
 let targetList;
+let player;
 
 
 function setup() {
   createCanvas(canvasW, canvasH);
   targetList = new TargetList(10);
+  player = new Player(10);
 }
 
 
 function draw() {
   background(10);
   targetList.draw();
+  player.draw();
+}
+
+
+function mouseClicked() {
+  targetList.checkHit(player.x, player.y, player.radius);
+}
+
+
+class Player {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get x() {
+    return mouseX;
+  }
+
+  get y() {
+    return mouseY;
+  }
+
+  draw() {
+    fill(200, 100, 30);
+    ellipse(mouseX, mouseY, this.radius * 2, this.radius * 2);
+  }
 }
 
 
@@ -49,15 +77,15 @@ class TargetList {
     this.maxRadius = 40;
     this.targets = []
     for (let i = 0; i < n; i++) {
-      const r = map(i, 0, n - 1, this.minRadius, this.maxRadius)
+      const r = map(i, 0, n - 1, this.minRadius, this.maxRadius);
 
       const target = new Target(
-        random() * width,
-        random() * height,
+        random(r, width - r),
+        random(r, height - r),
         r,
         random(100, 255)
       );
-      this.targets.push(target)
+      this.targets.push(target);
     }
   }
 
@@ -69,7 +97,7 @@ class TargetList {
 
   checkHit(x, y, radius) {
     for (let target of this.targets) {
-      target.isOverlapping(x, y, radius)
+      target.isOverlapping(x, y, radius);
     }
   }
 }
